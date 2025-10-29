fbd57b1ffe3c04ca5edfac361d7151312bb6ee41
pentcle <pentacle@pentacle.xyz>
2025-10-29 18:33:23 +0000

    fix linting

--- apps/curator-frontend/src/routes/components.tsx
+++ apps/curator-frontend/src/routes/components.tsx
@@ -1,21 +1,21 @@
-import {createFileRoute} from '@tanstack/react-router';
-import {EditProfile} from '@/components/edit-profile';
+import { createFileRoute } from '@tanstack/react-router';
+import { EditProfile } from '@/components/edit-profile';
 
 export const Route = createFileRoute('/components')({
-    component: RouteComponent,
+  component: RouteComponent,
 });
 
 function RouteComponent() {
-    return (
-        <article className="flex flex-col gap-8">
-            <h1>Component test page</h1>
-            <section className="box">
-                <EditProfile
-                    onSubmit={(data) => {
-                        console.log('Form submitted:', data);
-                    }}
-                />
-            </section>
-        </article>
-    );
+  return (
+    <article className="flex flex-col gap-8">
+      <h1>Component test page</h1>
+      <section className="box">
+        <EditProfile
+          onSubmit={(data) => {
+            console.log('Form submitted:', data);
+          }}
+        />
+      </section>
+    </article>
+  );
 }
