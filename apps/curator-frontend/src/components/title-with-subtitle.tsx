85d9449cd1f25c60da89737853bc611d8c96a9f7
walaa <walaa.ebrahim@nasca-sy.com>
2025-11-05 18:45:30 +0300

    stuck to tailwind foe flex-grow and export cn properly

--- apps/curator-frontend/src/components/title-with-subtitle.tsx
+++ apps/curator-frontend/src/components/title-with-subtitle.tsx
@@ -6,13 +6,9 @@ export default function TitleWithSubtitle({
   subtitle: string;
 }) {
   return (
-    <div className="flex flex-col items-start gap-3 w-full max-w-[42rem]" style={{ flexGrow: 0, flexShrink: 0 }}>
-      <h2 className="w-full max-w-[40rem] medium-title flex items-center text-dark-text" style={{ flexGrow: 0 }}>
-        {title}
-      </h2>
-      <p className="w-full flex items-center body-copy text-dark-text" style={{ flexGrow: 0 }}>
-        {subtitle}
-      </p>
+    <div className="flex flex-col items-start gap-3 w-full max-w-[42rem] flex-grow-0 flex-shrink-0">
+      <h2 className="w-full max-w-[40rem] medium-title flex items-center text-dark-text flex-grow-0">{title}</h2>
+      <p className="w-full flex items-center body-copy text-dark-text flex-grow-0">{subtitle}</p>
     </div>
   );
 }
